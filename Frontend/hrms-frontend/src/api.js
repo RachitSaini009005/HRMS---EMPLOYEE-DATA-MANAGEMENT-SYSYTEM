a53fa82664ec1e1948074de7fabde7bfc43b1e56
RachitSaini009005 <rachitsaini009005@gmail.com>
2026-02-20 23:26:44 +0530

    Fix project structure: removed root package.json and kept hrms-frontend

--- Frontend/hrms-frontend/src/api.js
+++ Frontend/hrms-frontend/src/api.js
@@ -1,5 +1,8 @@
-import axios from "axios"
+// import axios from "axios"
 
-export const API = axios.create({
-    baseURL : "http://127.0.0.1:8000/",
+// export const API = axios.create({
+//     baseURL : "http://127.0.0.1:8000/",
+// });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL 
 });
