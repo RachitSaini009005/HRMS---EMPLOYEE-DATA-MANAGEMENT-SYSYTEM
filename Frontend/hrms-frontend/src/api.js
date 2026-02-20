1d3e61b4928716a9a4224a28ebc2e8ffb297b8b5
RachitSaini009005 <rachitsaini009005@gmail.com>
2026-02-21 01:06:47 +0530

    api.js corrected

--- Frontend/hrms-frontend/src/api.js
+++ Frontend/hrms-frontend/src/api.js
@@ -3,6 +3,6 @@ import axios from "axios"
 // export const API = axios.create({
 //     baseURL : "http://127.0.0.1:8000/",
 // });
-const API = axios.create({
+export const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL 
 });
